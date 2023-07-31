bf17febecd10779fc0277367f4a767de6d064210
Miguel Nava <minsor2010@hotmail.com>
2023-07-31 12:12:55 -0400

    [ADD] New Module

---
+++ __manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Odoo Academy',
+    "summary":"""Module to handle courses and sessions""",
+    "description":"""Module to handle:
+        - Courses
+        - Sessions
+        - Attendees
+    """,
+    'license':'OPL-1',
+    'author': 'Miguel',
+    'website':'www.odoo.com',
+    'category': 'Custom Modules/Tech Training',
+    'depends':  ['base'],
+    'demo':  [''],
+    'data':  [''],
+    'application': True,
+}
