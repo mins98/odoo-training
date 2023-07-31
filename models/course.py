65ed143c05f8b686e269117112a896edc09253ff
Miguel Nava <minsor2010@hotmail.com>
2023-07-31 17:03:21 -0400

    [FIX] Module Course Error

--- models/course.py
+++ models/course.py
@@ -1,6 +1,19 @@
-from odoo import models
+from odoo import fields, models
 
 class Course(models.Model):
-    _name="academy.course"
+    _name="odoo_training.course"
     _description="Course info"
+    
+    #Campos reservados
+    name=fields.Char(string="Title", required=True)
+    active = fields.Boolean(string="active", default=True)
+    
+    #propios
+    description=fields.Text()
+    level=fields.Selection(string="Level",
+                           selection=[
+                               ('beginner','Beginner'),
+                               ('intermediate','Intermediate'),
+                               ('advance','Advance'),
+                           ], copy=False)
     
