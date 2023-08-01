1c6f5b0da8df3f2cd93e4876fc352839bb540347
Miguel Nava <minsor2010@hotmail.com>
2023-08-01 17:02:48 -0400

    [ADD] advance views

--- models/course.py
+++ models/course.py
@@ -20,13 +20,12 @@ class Course(models.Model):
     
     "Como la sesion en many2one aqui es one2many su opuesto"
     
-    sesion_ids=fields.One2many(comodel_name="odoo_training.session", string="Session", 
-                               inverse_name="course_id") 
+    sesion_ids=fields.One2many(comodel_name="odoo_training.session", string="Session", inverse_name="course_id") 
     "el inverse_name es el nombre del id de la relacion del otro modelo"
     
     currency_id = fields.Many2one(comodel_name="res.currency", string="Currency", default=lambda self:self.env.company.currency_id.id)
     base_price = fields.Monetary(string="Base Price", currency_field="currency_id")
-    additional_fee = fields.Monetary(string="Additional Fee", currency_field="currency_id")
+    additional_fee = fields.Monetary(string="Additional Fee", currency_field="currency_id", default="0")
     total_price = fields.Monetary(string="Total Price", currency_field="currency_id", compute="_compute_total_price", store=True)
     
     @api.depends("base_price", "additional_fee")
